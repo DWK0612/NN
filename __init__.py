b26ea9f81b1095523caf43875b7bbccb8656cd16
DWK0612 <107825985+DWK0612@users.noreply.github.com>
2022-06-20 13:43:55 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from .CNN import CNN, CNNConfig
+#from .BiLSTM import BiLSTM, BiLSTMConfig
+#from .BiLSTMCNN import BiLSTMCNN, BiLSTMCNNConfig
+#from .BiLSTMATTCNN import BiLSTMATTCNN, BiLSTMATTCNNConfig
+#from .BERT import BERT, BERTConfig
